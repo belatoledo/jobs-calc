bff48110651531032fc203684bb80c2fe6a1815d
Izabela Toledo <it.amancio@gmail.com>
2021-04-02 02:29:09 -0300

    Remaining calculation, update status and budget

--- src/routes.js
+++ src/routes.js
@@ -9,22 +9,77 @@ const profile  = {
   "monthly-budget": 3000,
   "days-per-week": 5,
   "hours-per-day": 5,
-  "vacation-per-year": 4 
+  "vacation-per-year": 4,
+  "value-hour": 75 
 }
 
-const jobs = []
+const jobs = [
+  {
+    id: 1,
+    name: "Pizzaria Guloso",
+    "daily-hours": 2,
+    "total-hours": 60,
+    created_at: Date.now()
+  },
+  {
+    id: 2,
+    name: "OneTwo Project",
+    "daily-hours": 3,
+    "total-hours": 47,
+    created_at: Date.now()
+  }
+]
+
+function remainingDays(job) {
+      //calculo de tempo restante
+      const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+      const createdDate = new Date(job.created_at)
+      const dueDay = createdDate.getDate() + Number(remainingDays)
+  
+      const dueDateInMs = createdDate.setDate(dueDay)
+  
+      const timeDiffInMs = dueDateInMs - Date.now()
+      //transformar milisegundos em dias
+      const dayInMs = 1000 * 60 * 60 * 24
+      const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+      //restam x dias
+      return dayDiff
+}
 
 //request, response
-routes.get('/', (req, res) => {res.render(views + "index")})
+routes.get('/', (req, res) => {
+
+  const updatedJobs = jobs.map((job) => {
+    //ajustes no job
+    const remaining = remainingDays(job)
+    const status = remaining <= 0 ? 'done' : 'progress'
+
+    return {
+      ...job,
+      remaining,
+      status,
+      budget: profile["value-hour"] * job["total-hours"]
+    }
+  })
+
+  return res.render(views + "index", { jobs: updatedJobs })
+
+})
 
 routes.get('/job', (req, res) => {res.render(views + "job")})
 routes.post('/job', (req, res) => {
   //req.body =  name: 'Orfanato Estrelinha', 'daily-hours': '4','total-hours': '24'
 
-  const job = req.body
-  job.createDat = Date.now() //atribuindo nova data 1:00 de video
+  const lastId = jobs[jobs.length - 1]?.id || 1;
 
-  jobs.push(req.body)
+  jobs.push({
+    id: lastId + 1,
+    name: req.body.name,
+    "daily-hours": req.body["daily-hours"],
+    "total-hours": req.body["total-hours"],
+    created_at: Date.now() //atribuindo data de hoje
+  })
   return res.redirect('/')
 })
 
